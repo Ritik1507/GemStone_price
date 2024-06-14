07ba1cd21fe5019135e52bc1769ac5ded9843565
Ritik1507 <112415038+Ritik1507@users.noreply.github.com>
2024-06-14 23:46:09 +0530

    template created

---
+++ template.py
@@ -0,0 +1,21 @@
+﻿import os
+from pathlib import Path
+
+package_name = "gemstone_price_prediction"
+
+list_of_files = [
+   
+   
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb",    
+    
+    
+]
